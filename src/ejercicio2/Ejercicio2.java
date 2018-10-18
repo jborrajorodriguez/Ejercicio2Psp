66196feba6705efd524e5bfcbaddea059cff0a4f
Juan Borrajo <juanborrajo10@gmail.com>
2018-10-18 09:13:42 +0200

    Fin proyecto

--- src/ejercicio2/Ejercicio2.java
+++ src/ejercicio2/Ejercicio2.java
@@ -9,7 +9,11 @@ public class Ejercicio2 {
 
     
     public static void main(String[] args) {
-        
+        new Hilo("a").start();
+        new Hilo("b").start();
+        new Hilo("c").start();
+        new Hilo("d").start();
+        System.out.println("Termina clase main");
     }
 
 }
